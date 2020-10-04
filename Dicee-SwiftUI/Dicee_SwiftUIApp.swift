2c7e5a9bd04afd0ab4787413c9f0d86d761ee649
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-10-04 16:57:22 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Maxim Bekmetov on 04.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
